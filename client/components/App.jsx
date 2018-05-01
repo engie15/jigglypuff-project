658ebf424c78924f27890aa0a9b68d3e2bb50f4b
Rosie Burkett <burkettroz@gmail.com>
2018-05-02 09:56:22 +1200

    add App.jsx routes and LandingPage.jsx functions

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,21 @@
+// NOTE: We will beautify this site once it functions.
+
 import React from 'react'
+import {HashRouter as Router, Route} from 'react-router-dom'
+
+import LandingPage from './LandingPage'
+import Login from './Login'
+import Register from './Register'
 
 const App = () => (
-  <div>
-    <h1>Kia ora beautiful world</h1>
-  </div>
+  <Router>
+    <div>
+      <h1>Kia ora beautiful world</h1>
+      <Route path = '/' component = {LandingPage} />
+      <Route path = '/login' component = {Login} />
+      <Route path = '/register' component = {Register} /> 
+    </div>
+  </Router>
 )
 
 export default App
---
+++ client/components/LandingPage.jsx
@@ -0,0 +1,30 @@
+// ----- Notes ----- //
+// If user aleady signed in, show the log out button only
+// If user is yet to sign in or register, show these buttons and a welcome message
+
+import React from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+//import {logoutUser} from '../actions/logout' // CHECK: name of action and file to match what Steve makes
+
+const LandingPage = () => (
+    <div>
+      {props.auth.isAuthenticated  // CHECK: name of prop matches action that Steve makes
+        // ~*~ If is logged in, show button to log out ~*~
+        ? <button onClick = {() => props.dispatch(logoutUser())}> Logout </button>  // CHECK: name of action
+        
+        // ~*~ If not logged in, show buttons to login or register ~*~
+        : <div>
+            <Link to = '/login'> Login </Link>
+            <Link to = '/register'> Register </Link>
+          </div>
+      }
+    </div>
+)
+
+// CHECK: This is taking in the current state of whether user is signed in. Want to understand exactly where this sits. Reducer? 
+const mapStateToProps = ({auth}) => {
+  return {auth}
+}
+
+export default connect(mapStateToProps) (LandingPage)
